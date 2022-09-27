ef93961b891412aecd31e80f1ebf27cab7acff43
krivtsov <krivtsov.su@gmail.com>
2022-09-27 22:11:09 +0400

    fix service review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -30,6 +30,6 @@ export class ReviewController {
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
-    return this.reviewService.findByProsuctId(productId); 
+    return this.reviewService.findByProductId(productId);
   }
 }
