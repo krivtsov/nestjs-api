57fc3240378ad983521ee73c0838c2bfb3786802
krivtsov <krivtsov.su@gmail.com>
2022-09-27 22:12:34 +0400

    add test review

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { disconnect } from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +21,9 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  afterAll(() => {
+    disconnect();
   });
 });
