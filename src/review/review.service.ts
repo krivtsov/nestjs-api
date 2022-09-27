51a383a156f6edd47ac26ce0e413b3f88f7655aa
krivtsov <krivtsov.su@gmail.com>
2022-09-27 22:50:25 +0400

    fix service

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types';
-import { Types } from 'mongoose';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewModel } from './review.model';
 
 @Injectable()
 export class ReviewService {
   constructor(
-    @InjectModel('Review') private readonly reviewModel: ModelType<ReviewModel>,
+    @InjectModel('Review')
+    private readonly reviewModel: ModelType<ReviewModel>,
   ) {}
 
   async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
