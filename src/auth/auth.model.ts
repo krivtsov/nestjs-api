37b0fc523350e024ba44b48012cb606526759426
krivtsov <krivtsov.su@gmail.com>
2022-09-27 15:51:16 +0400

    add auth model

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,15 @@
-export class AuthModel {
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface AuthModel extends Base {}
+@Schema()
+export class AuthModel extends TimeStamps {
+  @prop({ unique: true })
   email: string;
+
+  @prop()
   passwordHash: string;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
+import { AuthSchema } from './auth.model';
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Auth',
+        schema: AuthSchema,
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
