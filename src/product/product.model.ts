e2c8c2461f431f75e79263b7390b55d5f357b49c
krivtsov <krivtsov.su@gmail.com>
2022-09-27 15:51:39 +0400

    add product model

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,53 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: number;
-  advantages: string;
-  disAdvantages: string;
-  categories: string[];
-  tags: string;
-  characteristics: {
-    [key: string]: string;
-  };
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+  @prop()
+  name: string;
+
+  @prop()
+  value: string;
 }
+
+export interface ProductModel extends Base {}
+@Schema()
+export class ProductModel extends TimeStamps {
+  @prop()
+  image: string;
+
+  @prop()
+  title: string;
+
+  @prop()
+  price: number;
+
+  @prop()
+  oldPrice: number;
+
+  @prop()
+  credit: number;
+
+  @prop()
+  calculatedRating: number;
+
+  @prop()
+  description: number;
+
+  @prop()
+  advantages: string;
+
+  @prop()
+  disAdvantages: string;
+
+  @prop({ type: () => [String] })
+  categories: string[];
+
+  @prop({ type: () => [String] })
+  tags: string[];
+
+  @prop({ type: () => [ProductCharacteristic], _id: false })
+  characteristics: ProductCharacteristic[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './product.controller';
+import { ProductSchema } from './product.model';
 
 @Module({
   controllers: [ProductController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Product',
+        schema: ProductSchema,
+      },
+    ]),
+  ],
 })
 export class ProductModule {}
