ca3a96360052f36b0b71513543fff087a0b49df3
krivtsov <krivtsov.su@gmail.com>
2022-09-27 15:52:02 +0400

    add review model

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,24 @@
-export class ReviewModel {
-  _id: string;
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface ReviewModel extends Base {}
+@Schema()
+export class ReviewModel extends TimeStamps {
+  @prop()
   name: string;
+
+  @prop()
   title: string;
+
+  @prop()
   description: string;
+
+  @prop()
   rating: number;
+
+  @prop()
   createdAt: Date;
 }
+
+export const ReviewSchema = SchemaFactory.createForClass(ReviewModel);
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReviewSchema } from './review.model';
 
 @Module({
   controllers: [ReviewController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Review',
+        schema: ReviewSchema,
+      },
+    ]),
+  ],
 })
 export class ReviewModule {}
