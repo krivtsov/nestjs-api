e435f7f0522d95f197362bc27509c5c2f6b83ea0
krivtsov <krivtsov.su@gmail.com>
2022-10-09 16:48:08 +0400

    fix create product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -31,8 +31,8 @@ export class CreateProductDto {
   @IsNumber()
   credit: number;
 
-  @IsNumber()
-  description: number;
+  @IsString()
+  description: string;
 
   @IsString()
   advantages: string;
